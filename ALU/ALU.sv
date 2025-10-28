3b65767289d5bdfc6cbd7bf343c4ab88941681c2
MateoMor <mateomorales5558@gmail.com>
2025-10-28 16:46:19 -0500

    📝 Added localparams on ALU module

--- ALU/ALU.sv
+++ ALU/ALU.sv
@@ -7,19 +7,31 @@ module ALU(
     output logic signed [31:0] ALURes = 0
 );
 
+    // ALUOp
+    localparam  ALU_ADD = 4'b0000,
+                ALU_SUB = 4'b1000,
+                ALU_SLL = 4'b0001,
+                ALU_SLT = 4'b0010,
+                ALU_SLTU= 4'b0011,
+                ALU_XOR = 4'b0100,
+                ALU_SRL = 4'b0101,
+                ALU_SRA = 4'b1101,
+                ALU_OR  = 4'b0110,
+                ALU_AND = 4'b0111;
+
     // It executes each time any input changes
     always @* begin
         case(ALUOp)
-            4'b0000: ALURes = A + B; // ADD
-            4'b1000: ALURes = A - B; // SUB
-            4'b0001: ALURes = A << B; // SLL
-            4'b0010: ALURes = A < B; // SLT
-            4'b0011: ALURes = $unsigned(A) < $unsigned(B); // SLTU
-            4'b0100: ALURes = A ^ B; // XOR
-            4'b0101: ALURes = A >> B; // SRL
-            4'b1101: ALURes = A >>> B; // SRA
-            4'b0110: ALURes = A | B; // OR
-            4'b0111: ALURes = A & B; // AND
+            ALU_ADD: ALURes = A + B; // ADD
+            ALU_SUB: ALURes = A - B; // SUB
+            ALU_SLL: ALURes = A << B; // SLL
+            ALU_SLT: ALURes = A < B; // SLT
+            ALU_SLTU: ALURes = $unsigned(A) < $unsigned(B); // SLTU
+            ALU_XOR: ALURes = A ^ B; // XOR
+            ALU_SRL: ALURes = A >> B; // SRL
+            ALU_SRA: ALURes = A >>> B; // SRA
+            ALU_OR: ALURes = A | B; // OR
+            ALU_AND: ALURes = A & B; // AND
             4'b1001: ALURes = B;
         endcase
     end
