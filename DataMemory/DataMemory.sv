e5b898416818e13e77d100d7cf546cabbc07f1f7
MateoMor <mateomorales5558@gmail.com>
2025-11-17 11:15:01 -0500

    🐛 Added clk to solve error while writing

--- DataMemory/DataMemory.sv
+++ DataMemory/DataMemory.sv
@@ -1,10 +1,9 @@
 module DataMemory(
+    input logic clk,
     input logic [31:0] Address,
     input logic [31:0] DataWr,
-
     input logic DMWr,
     input logic [2:0] DMCtrl,
-
     output logic [31:0] DataRd
 );
 
@@ -17,52 +16,37 @@ module DataMemory(
                 SH   = 3'b001,
                 SW   = 3'b010;
 
-    // memory matrix
-    logic [7:0] dm [0:8192]; // 8 KiB = 64 Kbits
+    logic [7:0] dm [0:8191]; // 8 KiB
 
-    logic [7:0] byte0, byte1, byte2, byte3;
-
-    assign byte0 = dm[Address];
-    assign byte1 = dm[Address + 1];
-    assign byte2 = dm[Address + 2];
-    assign byte3 = dm[Address + 3];
-
-    // read logic
+    // Read
     always @* begin
         case (DMCtrl)
-            LB:      DataRd = {{24{byte0[7]}}, byte0};
-            LB_U:    DataRd = {24'b0, byte0};
-            LH:      DataRd = {{16{byte1[7]}}, byte1, byte0};
-            LH_U:    DataRd = {16'b0, byte1, byte0};
-            LW:      DataRd = {byte3, byte2, byte1, byte0};
+            LB:      DataRd = {{24{dm[Address][7]}}, dm[Address]};
+            LB_U:    DataRd = {24'b0, dm[Address]};
+            LH:      DataRd = {{16{dm[Address+1][7]}}, dm[Address+1], dm[Address]};
+            LH_U:    DataRd = {16'b0, dm[Address+1], dm[Address]};
+            LW:      DataRd = {dm[Address+3], dm[Address+2], dm[Address+1], dm[Address]};
             default: DataRd = 32'b0;
         endcase
     end
 
-    // write logic
-    always @(posedge DMWr) begin
-        case (DMCtrl)
-            SB: begin
-                dm[Address] <= DataWr[7:0];
-            end
-            SH: begin
-                dm[Address]   <= DataWr[7:0];
-                dm[Address+1] <= DataWr[15:8];
-            end
-            SW: begin
-                /* dm[Address]   <= DataWr[7:0];
-                dm[Address+1] <= DataWr[15:8];
-                dm[Address+2] <= DataWr[23:16];
-                dm[Address+3] <= DataWr[31:24]; */
-                dm[Address]   <= 2'b00; // Corregido
-                dm[Address+1] <= 2'b00;
-                dm[Address+2] <= 2'b00;
-                dm[Address+3] <= 2'b00;
-            end
-            default: begin
-                // No write operation
-            end
-        endcase
+    // Synchronous WRITE
+    always_ff @(posedge clk) begin
+        if (DMWr) begin
+            case (DMCtrl)
+                SB: dm[Address] <= DataWr[7:0];
+                SH: begin
+                    dm[Address]   <= DataWr[7:0];
+                    dm[Address+1] <= DataWr[15:8];
+                end
+                SW: begin
+                    dm[Address]   <= DataWr[7:0];
+                    dm[Address+1] <= DataWr[15:8];
+                    dm[Address+2] <= DataWr[23:16];
+                    dm[Address+3] <= DataWr[31:24];
+                end
+            endcase
+        end
     end
 
 endmodule
